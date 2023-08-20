523e1a647a34e7a46af5f422a141c99c75e42ba8
mahes <maheshdvp999@gmail.com>
2023-08-20 16:42:42 +0530

    roboshop scripts

--- 02-print.sh
+++ 02-print.sh
@@ -1,1 +1,16 @@
 echo hello world
+# Red --> 31
+#Green -->32
+#Yellow--> 33
+# BLUE --> 34
+#Magenta --> 35
+#Cyan --> 36
+# Syantax
+# echo -e "\e[CODEmMessage\e[0m"
+
+echo -e "\e[31mHello World\e[0m"
+echo -e "\e[32mHello World\e[0m"
+echo -e "\e[33mHello World\e[0m"
+echo -e "\e[34mHello World\e[0m"
+echo -e "\e[35mHello World\e[0m"
+echo -e "\e[36mHello World\e[0m"
