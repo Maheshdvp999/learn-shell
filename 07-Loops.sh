06c6f03e4910fe4c310d455c677448797aa3e940
mahes <maheshdvp999@gmail.com>
2023-08-24 15:25:49 +0530

    conditions

--- 07-Loops.sh
+++ 07-Loops.sh
@@ -6,4 +6,10 @@ a=10
 while [ $a -gt 0]; do
   echo Hello
   a=$(($a-1))
+  #break # this command can brake the loop
+done
+
+#Based on Inputs
+for comp in frontend catalogue user ; do
+  echo install component -$comp
 done
