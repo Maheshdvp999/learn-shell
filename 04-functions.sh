9c81f9b89645ed8a45a5f828efa68b2ced71ab19
mahes <maheshdvp999@gmail.com>
2023-08-24 15:12:47 +0530

    conditions

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,8 +1,22 @@
 greeting() {
   echo Helo , Good Morning
   echo welcome to DevOps Training
+  return 10
   echo Good to hear you
 }
 
 
 greeting
+echo Function Exit Status - $?
+
+#You declare var in main program , You can access that in Functions and vice versa
+#Function have its own special variables
+
+
+input() {
+  echo first Argument - $1
+  echo first Argument - $2
+  echo All Argument - $*
+  echo No of Arguments - $#
+}
+input abc 123
